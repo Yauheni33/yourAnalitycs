73b7b2f0fbade324a7f27e1be00d2bfd1366babf
Yauheni <busko-007@mail.ru>
2019-05-13 18:09:11 +0300

    made a bug

--- RoR_Project/app/javascript/components/About.jsx
+++ RoR_Project/app/javascript/components/About.jsx
@@ -3,9 +3,23 @@ import { BrowserRouter as Link } from 'react-router-dom'
 
 
 class About extends React.Component {
-    render() {
+    constructor(props) {
+        super(props);
+        this.state = { window: this.getAbout() }
+    }
+
+    getAbout() {
         return(
             <h2>About</h2>
+        )
+    }
+
+    render() {
+        console.log(this.props)
+        return(
+            <div>
+                { this.props.updateWindow(this.state.window) }
+            </div>
         )
     }
 }
