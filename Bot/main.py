e1c5f62d3794fbce66c1b42a87b21ae4e7ba0f0d
Yauheni <busko-007@mail.ru>
2019-05-14 18:47:27 +0300

    Fix bug routing

--- Bot/main.py
+++ Bot/main.py
@@ -2,7 +2,7 @@ import telebot
 import os
 import json
 
-bot = telebot.TeleBot('726788862:AAHSsms7pf7yNSoKGoje_vQmaUTR3hg1zWk')
+bot = telebot.TeleBot('####')
 
 
 @bot.message_handler(commands=["start"])
@@ -12,12 +12,12 @@ def start_command_handler(message):
 @bot.message_handler()
 def repeat_all_messages(message):
     url = message.text
-    filepath = os.path.join('/Users/yauheni/Desktop/antitroll/Django/ParseYoutubeRuby/todo', message.from_user.first_name + '.txt')
+    filepath = os.path.join('####', message.from_user.first_name + '.txt')
     with open(filepath, "w") as file:
         file.write(str(url))
     bot.send_message(message.chat.id, ('Ваш url обрабатывается, ожидайте :)'))
     while True:
-        path = '/Users/yauheni/Desktop/antitroll/Django/ParseYoutubeRuby/' + message.from_user.first_name + '.json'
+        path = '####' + message.from_user.first_name + '.json'
         if os.path.exists(path):
             #with open(path, 'r', encoding='utf-8') as file:
             #    data = json.loads(file.read(), encoding='utf-8')
--- RoR_Project/app/javascript/components/About.jsx
+++ RoR_Project/app/javascript/components/About.jsx
@@ -1,25 +1,10 @@
 import React from "react";
-import { BrowserRouter as Link } from 'react-router-dom'
 
 
 class About extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { window: this.getAbout() }
-    }
-
-    getAbout() {
+    render() {
         return(
             <h2>About</h2>
-        )
-    }
-
-    render() {
-        console.log(this.props)
-        return(
-            <div>
-                { this.props.updateWindow(this.state.window) }
-            </div>
         )
     }
 }
