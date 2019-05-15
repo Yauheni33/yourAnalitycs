6f58cb44d7fb8dea9420f26beaeede2007559183
Yauheni <busko-007@mail.ru>
2019-05-15 13:38:02 +0300

    add up added about us

--- RoR_Project/app/javascript/components/About.jsx
+++ RoR_Project/app/javascript/components/About.jsx
@@ -4,7 +4,72 @@ import React from "react";
 class About extends React.Component {
     render() {
         return(
-            <h2>About</h2>
+            <section id="about" className="about section">
+                <div className="container">
+                    <h2 className="title text-center">Что такое yourAnalytics?</h2>
+                    <p className="intro text-center">Наша платформа предназначена для Youtube-блогеров, как крупных(миллионников), так и небольших. Система, разработанная нами, анализирует Ваш канал по комментариям.</p>
+                    <p className="intro text-center">Как это работает?</p>
+                        <p className="intro text-center" style={{marginTop: "-45px", marginBottom: "200px"}}>Мы собираем комментарии с Ваших видеороликов, анализируем их путем сложных алгоритмов и выдаем полную и понятную статистику по каждому из Ваших видео.
+                    </p>
+                    <div className="row">
+                        <div className="item col-md-4 col-sm-6 col-xs-12">
+                            <div className="icon-holder">
+                                <i className="fa fa-heart"></i>
+                            </div>
+                            <div className="content">
+                                <h3 className="sub-title">Designed for developers</h3>
+                                <p>Outline a benefit here. Tell users what your plugin/software can do for them. You can change the icon above to any of the 400+ <a href="http://fortawesome.github.io/Font-Awesome/icons/" target="_blank">FontAwesome</a> icons available.</p>
+                            </div>
+                        </div>
+                        <div className="item col-md-4 col-sm-6 col-xs-12">
+                            <div className="icon-holder">
+                                <i className="fa fa-clock-o"></i>
+                            </div>
+                            <div className="content">
+                                <h3 className="sub-title">Time saver</h3>
+                                <p>Outline a benefit here. Tell users what your plugin/software can do for them. You can change the icon above to any of the 400+ <a href="http://fortawesome.github.io/Font-Awesome/icons/" target="_blank">FontAwesome</a> icons available.</p>
+                            </div>
+                        </div>
+                        <div className="item col-md-4 col-sm-6 col-xs-12">
+                            <div className="icon-holder">
+                                <i className="fa fa-crosshairs"></i>
+                            </div>
+                            <div className="content">
+                                <h3 className="sub-title">UX-centred</h3>
+                                <p>Outline a benefit here. Tell users what your plugin/software can do for them. You can change the icon above to any of the 400+ <a href="http://fortawesome.github.io/Font-Awesome/icons/" target="_blank">FontAwesome</a> icons available.</p>
+                            </div>
+                        </div>
+                        <div className="clearfix visible-md"></div>    
+                        <div className="item col-md-4 col-sm-6 col-xs-12">
+                            <div className="icon-holder">
+                                <i className="fa fa-tablet"></i>
+                            </div>
+                            <div className="content">
+                                <h3 className="sub-title">Mobile-friendly</h3>
+                                <p>Outline a benefit here. Tell users what your plugin/software can do for them. You can change the icon above to any of the 400+ <a href="http://fortawesome.github.io/Font-Awesome/icons/" target="_blank">FontAwesome</a> icons available.</p>
+                            </div>
+                        </div>
+                        <div className="item col-md-4 col-sm-6 col-xs-12">
+                            <div className="icon-holder">
+                                <i className="fa fa-code"></i>
+                            </div>
+                            <div className="content">
+                                <h3 className="sub-title">Easy to customise</h3>
+                                <p>Outline a benefit here. Tell users what your plugin/software can do for them. You can change the icon above to any of the 400+ <a href="http://fortawesome.github.io/Font-Awesome/icons/" target="_blank">FontAwesome</a> icons available.</p>
+                            </div>
+                        </div>
+                        <div className="item col-md-4 col-sm-6 col-xs-12">
+                            <div className="icon-holder">
+                                <i className="fa fa-coffee"></i>
+                            </div>
+                            <div className="content">
+                                <h3 className="sub-title">LESS files included</h3>
+                                <p>Outline a benefit here. Tell users what your plugin/software can do for them. You can change the icon above to any of the 400+ <a href="http://fortawesome.github.io/Font-Awesome/icons/" target="_blank">FontAwesome</a> icons available.</p>
+                            </div>
+                        </div>             
+                    </div>          
+                </div>
+            </section>
         )
     }
 }
