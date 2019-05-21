26c38f83fc2303d28fc49059abaa947826c50f20
Yauheni <busko-007@mail.ru>
2019-05-21 18:24:58 +0300

    Add up Features

--- RoR_Project/app/javascript/components/Features.jsx
+++ RoR_Project/app/javascript/components/Features.jsx
@@ -3,7 +3,21 @@ import React from "react";
 class Features extends React.Component {
     render() {
         return(
-            <h2>Features</h2>
+            <section id="features" className="features section">
+                <div className="container text-center">
+                    <h2 className="title">Features</h2>
+                    <ul className="feature-list list-unstyled">
+                        <li><i className="fa fa-check"></i> Fully responsive</li>
+                        <li><i className="fa fa-check"></i> HTML5 + CSS3</li>
+                        <li><i className="fa fa-check"></i> Built on <a href="http://getbootstrap.com/" target="_blank">Bootstrap 3.3</a></li>
+                        <li><i className="fa fa-check"></i> 400+ FontAwesome icons</li>
+                        <li><i className="fa fa-check"></i> 4 colour schemes</li>
+                        <li><i className="fa fa-check"></i> LESS files included</li>
+                        <li><i className="fa fa-check"></i> Compatible with all modern browsers</li>
+                    </ul>
+                </div>
+            </section>
+
         )
     }
 }
