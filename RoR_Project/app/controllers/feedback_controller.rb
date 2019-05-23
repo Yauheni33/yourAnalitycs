a0f069f71a70ff9729645aff8ab66f0b27bdfb6f
Yauheni <busko-007@mail.ru>
2019-05-23 14:33:34 +0300

    Fix bug with save in DB

--- RoR_Project/app/controllers/feedback_controller.rb
+++ RoR_Project/app/controllers/feedback_controller.rb
@@ -2,8 +2,6 @@ class FeedbackController < ApplicationController
   protect_from_forgery except: :create
 
   def create 
-    puts "ПАРАМЕТРЫ"
-    puts params.require(:feedback).permit(:firstName, :surname, :channel, :email)
     feedback = Feedback.new(feedback_params)
     if feedback.save
       render status: :created
@@ -15,6 +13,6 @@ class FeedbackController < ApplicationController
   private
 
   def feedback_params
-    params.require(:feedback).permit([:name, :surname, :name_channel, :email])
+    params.require(:feedback).permit(:name, :surname, :name_channel, :email)
   end
 end
